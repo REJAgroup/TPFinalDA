6e6265fdc4c7990b1c7cc3ab8c28715494ad738f
juanimartin01 <juanimartinnn3@gmail.com>
2021-09-25 19:12:45 -0300

    Cambio el app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
-import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 
