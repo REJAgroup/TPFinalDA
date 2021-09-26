4abcfcba2cf0754a7baeb08cf5bc798ff3468dc7
juanimartin01 <juanimartinnn3@gmail.com>
2021-09-26 05:16:13 -0300

    Actualice el angular y meti un carrusel

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
 import { MenuComponent } from './menu/menu.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 
 
 @NgModule({
   declarations: [
     NavComponent,
-    MenuComponent
+    MenuComponent,
+    CarouselComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     NavComponent,
-    MenuComponent
+    MenuComponent,
+    CarouselComponent
   ]
 })
 export class SharedModule { }
