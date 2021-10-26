6086cad77a0f5125b7607035cecc45c96ad3a6d7
Axel Ruiz <nahu.ruiz18@gmail.com>
2021-10-25 23:58:52 -0300

    login

---
+++ src/app/auth/pages/service/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {Usuario} from "../interfaces/Usuario";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl :string = "https://614ba83ee4cc2900179eb18b.mockapi.io/usuarios ";
+  constructor(private http: HttpClient) { }
+
+  login(usuario:string):Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(`${this.baseUrl}/?name=${usuario}`)
+  }
+}
